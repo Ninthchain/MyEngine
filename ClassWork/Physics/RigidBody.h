4259c1cb37b59996dab46e63c122c5b4cc4df08c
NorDev <half.life.1live@gmail.com>
2023-04-04 19:09:22 +0300

    First Commit

--- ClassWork/Physics/RigidBody.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GravityForce.h"
-
-class RigidBody
-{
-    GravityForce gravity;
-public:
-    
-};
---
+++ MyEngine/Geometry/geometry.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "./Line/Line.h"
+#include "./Point/Point.h"
+#include "./Vector/Vector.h"
